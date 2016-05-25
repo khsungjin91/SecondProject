0d12f26a6e1ebd865f9bbd02b81ffd16935ce81b
user1 <user1@KH1>
2016-05-25 14:37:15 +0900

    투자그래프

--- donjom/WebContent/manager/manager_investmn.jsp
+++ donjom/WebContent/manager/manager_investmn.jsp
@@ -60,71 +60,64 @@
         var chart = new google.visualization.PieChart(document.getElementById('piechart_2'));
         chart.draw(data, options);
       }
-
-      
       //3
       google.charts.setOnLoadCallback(drawChart_way);
       function drawChart_way() {
+    	
     	var user = document.userinput;
-    	var b = parseInt(user.b.value);
-    	var r = parseInt(user.r.value);
-    	var p = parseInt(user.p.value);
-    	var c = parseInt(user.c.value);
-   
+    	var men =  parseInt(user.gender_m.value);
+    	var women =  parseInt(user.gender_w.value);
         var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day',{ role: "style" }],
-          ["개인신용",  b, "blue"],
-          ["부동산",  r, "blue"],
-          ["사업자",  p, "blue"],
-          ["매출담보", c, "blue"]          
+          ['Task', 'Hours per Day'],
+          ["남자",  men],
+          ["여자",  women]
         ]);
-
         var options = {
-          title: '미정그래프',
+          title: '남녀 비율',
           is3D: false,
           fontSize: '15'
         };
-
         var chart = new google.visualization.PieChart(document.getElementById('piechart_3'));
         chart.draw(data, options);
       }
-
-
-      
      //bar 그래프
   
     google.charts.setOnLoadCallback(drawChart2);
     function drawChart2() {
     	
-    	var user = document.userinput;
-    	var twenty = parseInt(user.twenty.value);
-    	var thirty = parseInt(user.thirty.value);
-    	var forty = parseInt(user.forty.value);
-    	var fifty = parseInt(user.fifty.value);
-    	var sixty = parseInt(user.sixty.value);
-    	var seventy = parseInt(user.seventy.value);
-    	var noadult = parseInt(user.noadult.value);
+    	var m_noadult = ${dto.m_noadult};
+    	var m_twenty = ${dto.m_twenty};
+    	var m_thirty = ${dto.m_thirty};
+    	var m_forty = ${dto.m_forty};
+    	var m_fifty = ${dto.m_fifty};
+    	var m_sixty = ${dto.m_sixty};
+    	var m_eighty = ${dto.m_eighty};
+    	
+    	var w_noadult = ${dto.w_noadult};
+    	var w_twenty = ${dto.w_twenty};
+    	var w_thirty = ${dto.w_thirty};
+    	var w_forty = ${dto.w_forty};
+    	var w_fifty = ${dto.w_fifty};
+    	var w_sixty = ${dto.w_sixty};
+    	var w_eighty = ${dto.w_eighty};
     	
       var data = google.visualization.arrayToDataTable([                                                 
 		['나이', '남자', '여자'],
-		['80세이상', 40, 20],
-		['60-80', 0, 23.3],
-		['50-60', 10, 4.5],
-		['40-50', 10, 14.3],
-		['30-40', 10, 0.9],
-		['20-30', 20, 13.1],
-		['20세미만', 20, 13.1]
+		['80세이상', m_eighty, w_eighty],
+		['60-80', m_sixty, w_sixty],
+		['50-60', m_fifty, w_fifty],
+		['40-50', m_forty, w_forty],
+		['30-40', m_thirty, w_thirty],
+		['20-30', m_twenty, w_twenty],
+		['20세미만', m_noadult, w_noadult]
       ]);
-      
       var view = new google.visualization.DataView(data);
-      
       view.setColumns([0, 1,
                        { calc: "stringify",
                          sourceColumn: 1,
                          type: "string",
                          role: "annotation" },
                        2]);
-      
       var options_bar = {
     	        title: "연령대별 투자 현황",
     	        width: 1000,
@@ -132,15 +125,11 @@
     	        bar: {groupWidth: "55%"},
     	        legend: { position: "none" },
     	      };
-      
       var chart = new google.visualization.BarChart(document.getElementById('barchart_material'));
-
       chart.draw(view, options_bar);
     }
-     
-  </script>
-  
-  
+  </script> 
+ 
 <body>
 <h2> 투자 현황 상태</h2>
 
@@ -154,15 +143,10 @@
 <input type="hidden" value="${b}" id="b" name="b"/>
 <input type="hidden" value="${c}" id="c" name="c"/>
 <input type="hidden" value="${r}" id="r" name="m"/>
+<input type="hidden" value="${gender_m}" id="gender_m" name="gender_m"/>
+<input type="hidden" value="${gender_w}" id="gender_w" name="gender_w"/>
 <input type="hidden" value="${wayfull}" id="wayfull" name="wayfull"/>
 <input type="hidden" value="${waysplit}" id="waysplit" name="waysplit"/>
-<input type="hidden" value="${twenty}" id="twenty" name="twenty"/>
-<input type="hidden" value="${thirty}" id="thirty" name="thirty"/>
-<input type="hidden" value="${forty}" id="forty" name="forty"/>
-<input type="hidden" value="${fifty}" id="fifty" name="fifty"/>
-<input type="hidden" value="${sixty}" id="sixty" name="sixty"/>
-<input type="hidden" value="${seventy}" id="seventy" name="seventy"/>
-<input type="hidden" value="${noadult}" id="noadult" name="noadult"/>
 </form>
 
 <div id="piechart_1" style="width: 600px; height: 200px;"></div>
--- donjom/src/result/bean/TestBean.java
+++ donjom/src/result/bean/TestBean.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import manager.bean.Bar_ChartDto;
 import product.bean.InvestDto;
 
 @Controller
@@ -21,7 +22,7 @@ public class TestBean {
 	private ModelAndView mv;
 	
 	@RequestMapping("/manager_investmn2.dj")
-	public ModelAndView managerinvestmn(InvestDto dto){
+	public ModelAndView managerinvestmn(InvestDto dto,Bar_ChartDto chartDto){
 		//나이계산용
 		SimpleDateFormat format =  new SimpleDateFormat("yyyy");
 		//투자 전체 count
@@ -30,7 +31,8 @@ public class TestBean {
 		//투자 방법 0,1로 구분 
 		String [] way ={"0","1"};
 		//투자 방법 배열 2개 생성
-		int [] way_count = new int[way.length];
+		int [] way_count = new int[2];
+		int [] gender_count = new int[2];
 		
 		int p = 0;	int c = 0;	int b = 0;	int r = 0;
 		String p_code = "";
@@ -41,42 +43,51 @@ public class TestBean {
 		
 		String [] birth = new String[age_list.size()];
 		String [] gender = new String[gender_list.size()];
-		String [] genderation = new String[gender_list.size()]; 
 		int [] age = new int[age_list.size()];
-		int noadult = 0;
-		int twenty = 0;
-		int thirty = 0;
-		int forty = 0;
-		int fifty = 0;
-		int sixty = 0;
-		int eighty = 0;
+		int mnoadult=0; int wnoadult = 0;
+		int mtwenty = 0; int wtwenty = 0;
+		int mthirty = 0; int wthirty = 0;
+		int mforty = 0; int wforty = 0;
+		int mfifty = 0; int wfifty = 0;
+		int msixty = 0; int wsixty = 0;
+		int meighty = 0; int weighty = 0;
 		Date now = new Date();
 		String year = format.format(now);
-	
 		//age
-		for(int i = 0; i<age_list.size(); i++){
+
+	for(int i = 0; i<age_list.size(); i++){
 			birth[i] = (String) age_list.get(i);
 			String [] y = birth[i].split("");
 			String sum = y[0]+y[1]+y[2]+y[3];
 			age[i] = Integer.parseInt(year) - Integer.parseInt(sum)+1;
-			
-			
-			if(age[i] >= 0 && age[i] < 20){ noadult += 1 ;}
-			else if(age[i] >= 20 && age[i] < 30){ twenty += 1 ;}
-			else if(age[i] >= 30 && age[i] < 40){thirty += 1;}
-			else if(age[i] >= 40 && age[i] < 50){forty += 1;}
-			else if(age[i] >= 50 && age[i] < 60){fifty += 1;}
-			else if(age[i] >= 60 && age[i] < 70){sixty += 1;}
-			else if(age[i] >= 70){eighty += age[i];}
+
+		if(gender_list.get(i).equals("men")){
+			gender_count[0] += 1;
+			if(age[i] >= 0 && age[i] < 20){mnoadult += 1;  chartDto.setM_noadult(mnoadult) ;}
+			else if(age[i] >= 20 && age[i] < 30){mtwenty += 1; chartDto.setM_twenty(mtwenty) ;}
+			else if(age[i] >= 30 && age[i] < 40){mthirty += 1; chartDto.setM_thirty(mthirty);}
+			else if(age[i] >= 40 && age[i] < 50){mforty += 1; chartDto.setM_forty(mforty);}
+			else if(age[i] >= 50 && age[i] < 60){mfifty += 1; chartDto.setM_fifty(mfifty);}
+			else if(age[i] >= 60 && age[i] < 70){msixty += 1; chartDto.setM_sixty(msixty);}
+			else if(age[i] >= 70){meighty += age[i]; chartDto.setM_eighty(meighty);}
+	
+		}else{
+			gender_count[1] += 1;
+			if(age[i] >= 0 && age[i] < 20){ wnoadult += 1 ; chartDto.setW_noadult(wnoadult);}
+			else if(age[i] >= 20 && age[i] < 30){ wtwenty += 1 ; chartDto.setW_twenty(wtwenty);}
+			else if(age[i] >= 30 && age[i] < 40){wthirty += 1; chartDto.setW_thirty(wthirty);}
+			else if(age[i] >= 40 && age[i] < 50){wforty += 1; chartDto.setW_forty(wforty);}
+			else if(age[i] >= 50 && age[i] < 60){wfifty += 1; chartDto.setW_fifty(wfifty);}
+			else if(age[i] >= 60 && age[i] < 70){wsixty += 1; chartDto.setW_sixty(wsixty);}
+			else if(age[i] >= 70){weighty += age[i]; chartDto.setW_eighty(weighty);}
 		}
-		
+	}
 		//way
 		for(int i = 0; i<way.length ; i++){
 		dto.setI_way(way[i]);
 		// 0 과 1 번 각각의 투자 방법의 count를 가져온다.
 		way_count[i] = (int) sqlMap.queryForObject("result.investway", dto.getI_way());
 		}
-		
 		//category
 		for(int i = 0; i<count ; i++){
 			p_code = (String) code_list.get(i);
@@ -87,17 +98,13 @@ public class TestBean {
 			if(category[i].equals("P"))	{p += category[i].indexOf("P")+1;}
 			if(category[i].equals("C"))	{c += category[i].indexOf("C")+1;}
 		}
-		
+
 		mv.addObject("count", count);
-		mv.addObject("noadult",noadult);
-		mv.addObject("twenty",twenty);
-		mv.addObject("thirty",thirty);
-		mv.addObject("forty",forty);
-		mv.addObject("fifty",fifty);
-		mv.addObject("sixty",sixty);
-		mv.addObject("seventy",eighty);
+		mv.addObject("dto", chartDto);
 		mv.addObject("wayfull", way_count[0]);
 		mv.addObject("waysplit", way_count[1]);
+		mv.addObject("gender_m", gender_count[0]);
+		mv.addObject("gender_w", gender_count[1]);
 		mv.addObject("p", p);
 		mv.addObject("b", b);
 		mv.addObject("c", c);
