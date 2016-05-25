3af20ddee3b54f8468ba0ef66f7d9c3a9281ae1e
user1 <user1@KH1>
2016-05-25 15:09:27 +0900

    별로 한것이 없군;;;

--- donjom/WebContent/news/news_manager.jsp
+++ donjom/WebContent/news/news_manager.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -25,8 +26,9 @@
 		<tr>
 			<td> ${list.site} </td>
 		</tr>
+		
 	</table>
-	<input type="submit" value="뉴스 올리기">
+	<input type="button" value="뉴스 올리기" onclick="javascript:location.href='?currentPage= value="currentPage"/>';">
 	<input type="button" value="뉴스 수정하기" onclick="javascript:location.href='/news_writePro.dj'">
 	<input type="button" value="뉴스 삭제" onclick="javascrip:location.href='/news_deletePro.dj'">
 	</form>
--- donjom/src/manager/bean/CompanyBean.java
+++ donjom/src/manager/bean/CompanyBean.java
@@ -1,5 +1,6 @@
 package manager.bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,23 +19,29 @@ public class CompanyBean {
 	private SqlMapClientTemplate sqlMap;
 	@Autowired
 	private ModelAndView mv;
+	@SuppressWarnings("unused")
+	private int currentPage = 1; //현재페이지
+	@SuppressWarnings("unused")
+	private int totalCount;		 // 총 게시물의 수
+	@SuppressWarnings("unused")
+	private int blockCount = 10; // 한 페이지의 게시물의 수
+	@SuppressWarnings("unused")
+	private int blockPage = 5;	 // 한 화면에 보여줄 페이지의 수
+	@SuppressWarnings("unused")
+	private String pagingHtml;	 // 페이징을 구현한 HTML
 	
 	
 	// 회사정보 변경폼
 	@RequestMapping("/manager_companyinfo.dj")
 	public ModelAndView companyinfo(ManagerCompanyDto company){
-		
-		mv.setViewName("company");	
 		List list = sqlMap.queryForList("companyList", company);
 		mv.addObject("list", list);
 		mv.setViewName("/managerpage/manager_companyinfo.jsp");
 		return mv;
 	}
-	
 	// 회사정보 저장
 	@RequestMapping("/manager_companyPro.dj") 
 	public ModelAndView companyPro(ManagerCompanyDto company){
-		
 		// input 입력 
 		mv.setViewName("company");
 		// db 저장
@@ -44,11 +51,9 @@ public class CompanyBean {
 		mv.setViewName("/managerpage/manager_companyPro.jsp");
 		return mv;
 	}
-	
 	// 회사정보 수정
 	@RequestMapping("/manager_companyModify.dj")
 	public ModelAndView companyModify(HttpServletRequest request, HttpServletResponse response, ManagerCompanyDto company){
-		
 		// 저장된 dto불러옴
 		List list = sqlMap.queryForList("companyList", company);
 		mv.setViewName("company");
@@ -60,6 +65,7 @@ public class CompanyBean {
 		return mv;
 	}
 	
+	
 	// 메인에서 보여지는 뉴스페이지
 	@RequestMapping("/news_list.dj")
 	public ModelAndView newslist(){
@@ -67,11 +73,24 @@ public class CompanyBean {
 		return mv;
 	}
 	// 관리자 모드에서 보여지는 뉴스페이지
+	@SuppressWarnings("unchecked")
 	@RequestMapping("/news_manager.dj")
-	public ModelAndView newsmanager(){
-		//List list = sqlMap.queryForList("newsList", null);
-		//mv.addObject("list", list);
+	public ModelAndView newsmanager(ManagerPageingDto dto){
+		@SuppressWarnings("unused")
+		List<ManagerNewsDto> list = sqlMap.queryForList("newsList", dto);
+		
+		totalCount = list.size(); // 전체 글 갯수
+		dto = new ManagerPageingDto(currentPage, totalCount, blockCount, blockPage, totalCount);
+		pagingHtml = dto.getPagingHtml().toString();
+		int lastCount = totalCount;
+		
+		if(dto.getEndCount() < totalCount)
+			lastCount = dto.getEndCount() + 1;
+		
+		list = list.subList(dto.getStartCount(), lastCount);
+		
 		mv.setViewName("/news/news_manager.jsp");
+		
 		return mv;
 	}
 	// 뉴스올리기
@@ -82,7 +101,7 @@ public class CompanyBean {
 	}
 	// 뉴스 수정
 	@RequestMapping("/news_writePro.dj")
-	public ModelAndView newswritePro(Manager_NewsDto news){
+	public ModelAndView newswritePro(ManagerNewsDto news){
 		mv.setViewName("news");
 		sqlMap.insert("newsin", news);
 		List list = sqlMap.queryForList("newsList", null);
